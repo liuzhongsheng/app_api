5f4c361d27f95c2c050bc225fdd54e7ef4e467e5
liuzhongsheng <996674366@qq.com>
2020-07-24 16:14:51 +0800

    更新文档

--- config/ExportApiDocConfig.php
+++ config/ExportApiDocConfig.php
@@ -2,18 +2,13 @@
 return [
         'start_using'   =>  'on',  //on 开 off关闭
         'doc_dir'       => 'Word',  //文档保存目录默认
-        'word_name'     => '纤丽兰心接口文档',
-        'test_url'      => 'https://qllxapi.apiself.com',
+        'word_name'     => '接口文档',
+        'test_url'      => 'https://xxxx',
         'api_dir_path'  => [
             
             //通用
             'api'  => [
-                'Api/actions/Index/v1',
-                'Api/actions/Wechat/v1',
-                'Api/actions/Activity/v1',
-                'Api/actions/Order/v1',
-                'Api/actions/Store/v1',
-                'Api/actions/User/v1',
+                'Api/actions/Init/v1',
             ]
         ],
         'v1.0'          => [
